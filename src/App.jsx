e7dd2d6ec50fec09b8fea0a39d0c34cc5d40c920
Asat1llo <asatilloiminjonov57@gmail.com>
2024-01-27 15:59:38 +0300

    changed basic directions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import {Header, Footer} from './components/index.jsx'
+import { Home, Movie } from './pages/index.js'
 
-import Header from './components/header/header.jsx'
-import Footer from './components/footer/footer.jsx'
-import Home from './pages/home/home.jsx'
-import Movie from './pages/movie/movie.jsx'
 
 function App() {
 
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import classes from './home.module.css'
+const Home = () => {
+    return (
+        <>
+            <h3 className={classes.homeTitle}>this is home page</h3>
+        </>
+    )
+}
+
+export default Home
